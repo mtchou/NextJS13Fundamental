770a7aae2def47282ca151c70bb65877c682b4b1
mtchou <48898155+mtchou@users.noreply.github.com>
2023-02-03 10:57:09 +1100

    feat: create pages and routes, and modify prettierignore

---
+++ app/conference/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <>
+      <h1>Welcome to Conference</h1>
+      <h2>
+        {" "}
+        <Link href="/conference/speakers">Speakers</Link>
+      </h2>
+      <h2>
+        {" "}
+        <Link href="/conference/sessions">Sessions</Link>
+      </h2>
+    </>
+  );
+}
